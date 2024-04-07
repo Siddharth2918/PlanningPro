7ea1cd3ae3df99792c4ac5923fe0ad968c5752c4
Siddharth S. Singh <91372571+Siddharth2918@users.noreply.github.com>
2024-04-07 19:46:59 +0530

    Version 1.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import AllPlans from './components/AllPlans';
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <AllPlans/>
+    </>
+  )
+}
+
+export default App
